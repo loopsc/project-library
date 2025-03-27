9097b87be9f991a238d1c6d697341a92990b8444
loops <128599880+loopsc@users.noreply.github.com>
2025-03-27 23:08:43 +1300

    More minor layout changes and fixed checkbox value
    Changed more font sizing to be responsive such as page header and some button and labels in the form.
    Changed box shadow to have a neon design.
    When submiting the form, read status of a book now reflects correctly in the library entry.

--- script.js
+++ script.js
@@ -60,12 +60,24 @@ const book2 = new Book("1984", "George Orwell", 328, true);
 const book3 = new Book("To Kill a Mockingbird", "Harper Lee", 281, false);
 const book4 = new Book("Dune", "Frank Herbert", 412, false);
 const book5 = new Book("The Catcher in the Rye", "J.D. Salinger", 277, true);
+const book6 = new Book("Pride and Prejudice", "Jane Austen", 432, true);
+const book7 = new Book("Moby-Dick", "Herman Melville", 635, false);
+const book8 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+const book9 = new Book("War and Peace", "Leo Tolstoy", 1225, false);
+const book10 = new Book("The Odyssey", "Homer", 560, true);
+const book11 = new Book("Brave New World", "Aldous Huxley", 311, true);
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
 addBookToLibrary(book5);
+addBookToLibrary(book6);
+addBookToLibrary(book7);
+addBookToLibrary(book8);
+addBookToLibrary(book9);
+addBookToLibrary(book10);
+addBookToLibrary(book11);
 
 displayLibrary()
 
@@ -82,7 +94,6 @@ const formInputRead = document.getElementById("form_read_status");
 
 showDialogButton.addEventListener('click', () => {
     dialog.showModal()
-    clearFormInputs()
 })
 
 closeButton.addEventListener('click', (e) => {
@@ -99,7 +110,7 @@ form.addEventListener("submit", (e) => {
     title = formInputTitle.value;
     author = formInputAuthor.value;
     pages = formInputPages.value;
-    read = formInputRead.value;
+    read = formInputRead.checked;
 
     const book = new Book(title, author, pages, read)
 
@@ -110,12 +121,3 @@ form.addEventListener("submit", (e) => {
     form.reset()
     dialog.close()
 })
-
-function clearFormInputs() {
-    formInputTitle.textContent = ''
-    formInputAuthor.textContent = ''
-    formInputPages.textContent = ''
-    formInputRead.textContent = ''
-
-    console.log('cleared inputs')
-}
